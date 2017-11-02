f7044ccde0a97cd060ec61f5c2cf177ec4718553
juanjuar457 <testjj4573421@gmail.com>
2017-11-02 09:26:17 -0700

    added state to ListContacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -3,9 +3,90 @@ import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
 
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    };
 
+    state = {
+        query: ''
+    };
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim() })
+    };
+
+    clearQuery = () => {
+        this.setState({ query: ''}) //resets the query
+    };
+
+    render() {
+        const { contacts, onDeleteContact } = this.props;
+        const { query } = this.state;
+        //KIP the obj destructuring! - line 74, 76,78,95,109
+        //Looks like the obj destructuring is just adding the this.props OR
+        //- adding the this.state - looks  a little cleaner vs always having
+        //the same call multiple times just have the obj property name
+
+        let showingContacts;
+        if (query) {
+            const match = new RegExp(escapeRegExp(this.state.query), 'i');
+            showingContacts = contacts.filter((contact) => match.test(contact.name))
+        }else {
+            showingContacts = contacts
+        }
+
+        showingContacts.sort(sortBy('name'));
+
+        return (
+            <div className='list-contacts'>
+                {/*{JSON.stringify(this.state)}
+                used to show updated query state
+                */}
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search contacts'
+                        value={query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+
+                {showingContacts.length !== contacts.length && (
+                    <div className='showing-contacts'>
+                        <span> Now showing {showingContacts.length} of {contacts.length} total </span>
+                        <button onClick={this.clearQuery}>Show All</button>
+                    </div>
+                )}
+
+                <ol className='contact-list'>
+                    {showingContacts.map((contact) => (
+                        <li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}/>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default ListContacts
 // import React from 'react';
-
+//>>>>>>>KIP THAT DEPENDING ON THE TYPE OF COMPONENT YOU ARE USING WILL DETERMINE THE TYPE OF IMPORTS-
+// YOU WILL USE AT THE TOP OF YOUR DOC
+//****** EXAMPLE OF LISTCONTACTS AS CLASS COMPONENT *******
 // class ListContacts extends Component {
 //     render() {
 //         return(
@@ -30,6 +111,7 @@ import sortBy from 'sort-by';
 //     }
 // }
 
+//********LIST CONTACTS AS A STATELESS FUNCTIONAL COMPONEN**********
 // function ListContacts (props) {
 //     return(
 //         <ol className='contact-list'>
@@ -51,63 +133,3 @@ import sortBy from 'sort-by';
 //         </ol>
 //     )
 // }
-
-class ListContacts extends Component {
-    static propTypes = {
-        contacts: PropTypes.array.isRequired,
-        onDeleteContact: PropTypes.func.isRequired
-    };
-
-    state = {
-        query: ''
-    };
-
-    updateQuery = (query) => {
-        this.setState({ query: query.trim() })
-    };
-
-    render() {
-        let showingContacts;
-        if (this.state.query) {
-            const match = new RegExp(escapeRegExp(this.state.query), 'i');
-            showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
-        }else {
-            showingContacts = this.props.contacts
-        }
-
-        showingContacts.sort(sortBy('name'));
-
-        return (
-            <div className='list-contacts'>
-                {/*{JSON.stringify(this.state)}*/}
-                <div className='list-contacts-top'>
-                    <input
-                        className='search-contacts'
-                        type='text'
-                        placeholder='Search contacts'
-                        value={this.state.query}
-                        onChange={(event) => this.updateQuery(event.target.value)}
-                    />
-                </div>
-                <ol className='contact-list'>
-                    {showingContacts.map((contact) => (
-                        <li key={contact.id} className='contact-list-item'>
-                            <div className='contact-avatar' style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}/>
-                            <div className='contact-details'>
-                                <p>{contact.name}</p>
-                                <p>{contact.email}</p>
-                            </div>
-                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
-                                Remove
-                            </button>
-                        </li>
-                    ))}
-                </ol>
-            </div>
-        )
-    }
-}
-
-export default ListContacts
